413ffa0e112d58c4d1327aca4db229cca1203355
Kan <kantippayamontri@gmail.com>
2020-01-09 17:16:42 +0700

    nothing

--- lib/datamanager.dart
+++ lib/datamanager.dart
@@ -153,7 +153,6 @@ class UseString {
   static String motorliscense = "Driveliscense Motorcycle";
   static String profilede = "Profile detail";
   static String logout = "Log Out";
-
   static String motorfront = "Motorcycle front picture";
   static String motorleft = "Motorcycle left picture";
   static String motorright = "Motorcycle right picture";
@@ -162,6 +161,7 @@ class UseString {
   static String picuploadval = "Please select all images.";
   static String close = "Close";
   static String registermotorfail = "Fail to register motorcycle.";
+   static String waitingforrent = "Waiting for rent";
 
 }
 
@@ -194,8 +194,8 @@ class GearMotor {
 }
 
 class CarStatus{
-  static String nothing = "NOTHING";
-  static String waiting = "WAITING";
-  static String booked = "BOOKED";
-  static String working = "WORKING";
+  static const String nothing = "NOTHING";
+  static const String waiting = "WAITING";
+  static const String booked = "BOOKED";
+  static const String working = "WORKING";
 }
---
+++ lib/page/motordetailpage.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pickcar/bloc/motordetail/motordetailbloc.dart';
+import 'package:pickcar/bloc/motordetail/motordetailevent.dart';
+import 'package:pickcar/bloc/motordetail/motordetailstate.dart';
+import 'package:pickcar/main.dart';
+import 'package:pickcar/page/rental/motorrentaldefindpage.dart';
+
+class MotorDetailPage extends StatefulWidget {
+  String motordocid;
+  MotorDetailPage({@required this.motordocid}) {
+    print("MotorDetailPage motordocid : ${motordocid}");
+  }
+  @override
+  _MotorDetailPageState createState() => _MotorDetailPageState();
+}
+
+class _MotorDetailPageState extends State<MotorDetailPage> {
+  MotorDetailBloc _motorDetailBloc;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    print("motor firestore docid ; ${widget.motordocid}");
+    _motorDetailBloc =
+        MotorDetailBloc(context: context, firestoredocid: widget.motordocid);
+    _motorDetailBloc.add(MotorDetailLoadData());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      height: MediaQuery.of(context).size.height -
+          MediaQuery.of(context).padding.top,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+      child: BlocBuilder(
+        bloc: _motorDetailBloc,
+        builder: (BuildContext context, state) {
+          if (state is MotorDetailStartState) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (state is MotorDetailShowdata) {
+            return Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("${state.motorcycle.carstatus}"),
+                  RaisedButton(
+                    child: Text("${state.motorcycle.firestoredocid}"),
+                    onPressed: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //       builder: (context) => MotorRentalDefindPage(
+                      //           motorcycle: _motorDetailBloc.motorcycle)),
+                      // );
+                    },
+                  ),
+                ],
+              ),
+            );
+          }
+        },
+      ),
+    ));
+  }
+}
