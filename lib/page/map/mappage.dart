13fc5ab1aefe348f75471c73ee75daafeb6edba8
590610661 weerasak chiangsoi <jame25193@gmail.com>
2020-01-18 21:41:30 +0700

    basic map

--- lib/page/map/mappage.dart
+++ lib/page/map/mappage.dart
@@ -1,24 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:pickcar/datamanager.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'dart:async';
 
-class MapPage extends StatelessWidget {
+import 'package:pickcar/datamanager.dart';
+class MapPage extends StatefulWidget {
+  double zoom = 16;
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  Completer<GoogleMapController> _controller = Completer();
+  Future<LocationData> getCurrentLocation() async {
+    Location location = Location();
+      return await location.getLocation();
+  }
+  Future getlocationnow() async {
+    LocationData currentLocation;
+    final GoogleMapController controller = await _controller.future;
+    currentLocation = await getCurrentLocation();
+    print(currentLocation.latitude.toString()+' '+currentLocation.longitude.toString());
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+    CameraPosition(
+      target: LatLng(
+          currentLocation.latitude,
+          currentLocation.longitude),
+      zoom: 16,
+    )));
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
+    var data = MediaQuery.of(context);
+    getlocationnow();
+    return  Scaffold(
+      body: Container(
+        width: data.size.width,
+        height: data.size.height,
+        child: Stack(
           children: <Widget>[
-            Text("mappage"),
-            RaisedButton(
-              onPressed: (){
-                Navigator.of(context).pushNamed(Datamanager.listcar);
-                // Navigator.of(context).pushNamed(Datamanager.detailsearch);
+             GoogleMap(
+              mapType: MapType.normal,
+              zoomGesturesEnabled: true,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(13.7650836, 100.5379664),
+                zoom: widget.zoom,
+              ),
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
               },
-              child: Text("search"),
             ),
           ],
         ),
-      ),
+      )
     );
   }
 }
